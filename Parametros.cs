f5d3221e47415015591a6bb4c8bcaec888a65fec
eltonfd@gmail.com <eltonfd@gmail.com>
2020-08-27 16:10:52 -0300

    first commit

---
+++ Parametros.cs
@@ -0,0 +1,26 @@
+namespace RelaxAndFix_cs
+{
+  //classe com todos os parâmetros
+  public class Parametros
+  {
+    public int crew { get; set; }
+    public int task { get; set; }
+    public int date { get; set; }
+    public int competencies { get; set; }
+    public int technical_place { get; set; }
+    public double hours_per_shift { get; set; }
+    public int level_total { get; set; }
+    public double[] c { get; set; }
+    public int[] d1 { get; set; }
+    public int[] d2 { get; set; }
+    public int[,] e { get; set; }
+    public int[,] bo { get; set; }
+    public int[,] bm { get; set; }
+    public int[,] bm2 { get; set; }
+    public int[,] bm3 { get; set; }
+    public int[,] tp { get; set; }
+    public double[,] tr { get; set; }
+    public double[] tm { get; set; }
+    public int[] g { get; set; }
+  }
+}
