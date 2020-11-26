556308dd310db2abbb2dde9f612312fda523cee8
eltonfd@gmail.com <eltonfd@gmail.com>
2020-11-25 21:39:17 -0300

    Adicionado script para automatizar testes

---
+++ Resultado.cs
@@ -0,0 +1,10 @@
+namespace RelaxAndFix_cs
+{
+  //classe com todos os parâmetros
+  public class Resultado
+  {
+    public string nome { get; set; }
+    public long segundos { get; set; }
+    public double valor { get; set; }
+  }
+}
